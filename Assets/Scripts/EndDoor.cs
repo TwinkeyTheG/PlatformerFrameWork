689d3594b2da7fbae514918b1fef9c244f8e32d8
TwinkeyTheG <conner.vergara@digipen.edu>
2022-02-01 17:59:54 -0800

    made some changes to base level

--- Assets/Scripts/EndDoor.cs
+++ Assets/Scripts/EndDoor.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class EndDoor : MonoBehaviour
 {
-    public string LevelToLoad;
+    public string LevelToLoad = "EndScene";
     // Start is called before the first frame update
     void Start()
     {
